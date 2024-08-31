8af6e31dd31e771273d0e21a857c1f25bea26330
Jasmine Iwanek <jasmine@iwanek.co.uk>
2024-08-31 16:35:01 -0400

    Fix duplicate shuttle386sx

--- src/machine/m_at_286_386sx.c
+++ src/machine/m_at_286_386sx.c
@@ -521,7 +521,7 @@ machine_at_shuttle386sx_init(const machine_t *model)
     machine_at_common_init(model);
 
     device_add(&intel_82335_device);
-    device_add(&keyboard_at_ami_device);
+    device_add_params(machine_get_kbc_device(machine), (void *) model->kbc_params);
 
     if (fdc_current[0] == FDC_INTERNAL)
         device_add(&fdc_at_device);
@@ -543,29 +543,6 @@ machine_at_adi386sx_init(const machine_t *model)
 
     machine_at_common_init_ex(model, 2);
     device_add(&amstrad_megapc_nvr_device); /* NVR that is initialized to all 0x00's. */
-
-    device_add(&intel_82335_device);
-    device_add_params(machine_get_kbc_device(machine), (void *) model->kbc_params);
-
-    if (fdc_current[0] == FDC_INTERNAL)
-        device_add(&fdc_at_device);
-
-    return ret;
-}
-
-int
-machine_at_shuttle386sx_init(const machine_t *model)
-{
-    int ret;
-
-    ret = bios_load_interleaved("roms/machines/shuttle386sx/386-Shuttle386SX-Even.BIN",
-                                "roms/machines/shuttle386sx/386-Shuttle386SX-Odd.BIN",
-                                0x000f0000, 131072, 0);
-
-    if (bios_only || !ret)
-        return ret;
-
-    machine_at_common_init(model);
 
     device_add(&intel_82335_device);
     device_add_params(machine_get_kbc_device(machine), (void *) model->kbc_params);
